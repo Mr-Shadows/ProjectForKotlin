87f9a7bbb971cb01d1c94150d3e46bd099412b3d
Oleg <oleg-kruglenko@mail.ru>
2024-10-03 18:28:51 +0300

    create Mobile Develop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
+package me.fleks.moderandroidapp
+
+//import android.support.v7.app.AppCompactActivity
+//import android.os.Bundle
 fun main(args: Array<String>) {
     println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
+//Kirill, this is shit !
+
