7c335b04c69ba1344be1a082597242d746799be2
Oleg <oleg-kruglenko@mail.ru>
2024-10-04 13:56:51 +0300

    re-creation of the project

--- src/main/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-package me.fleks.moderandroidapp
-
-//import android.support.v7.app.AppCompactActivity
-//import android.os.Bundle
-fun main(args: Array<String>) {
-    println("Hello World!")
-}
-//Kirill, this is shit !
-
